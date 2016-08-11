3a1f0d87a49a9e76d95f91fe4a6a2e48cc7d7ded
Benjamin David Lunt <spamcollector@cableone.net>
2016-08-10 21:25:38 -0700

    Add files via upload

--- misc/makehdr.c
+++ misc/makehdr.c
@@ -2,7 +2,7 @@
 *  Copyright (c) 1984-2016    Forever Young Software  Benjamin David Lunt  *
 *                                                                          *
 *                            FYS OS version 2.0                            *
-* FILE: loader.c                                                           *
+* FILE: makehdr.c                                                          *
 *                                                                          *
 * This code is freeware, not public domain.  Please use respectfully.      *
 *                                                                          *
@@ -25,12 +25,13 @@
 * DESCRIPTION:                                                             *
 *  Loader code for the FYS OS version 2.0 operating system.                *
 *                                                                          *
-* BUILT WITH:   LCC                                                        *
-*             https://www.cs.virginia.edu/~lcc-win32/                      *
+* BUILT WITH:                                                              *
+*      LCC    https://www.cs.virginia.edu/~lcc-win32/                      *
 *          Command line: lcc makehdr.c<enter>                              *
 *                        lcclnk makehdr.obj<enter>                         *
-*                                                                          *
-*   (or any compiler that will make a DOS like app)                        *
+*   or                                                                     *
+*      DJGPP  http://www.delorie.com/djgpp/                                *
+*          Command line: gcc -Os makehdr.c -o makehdr.exe -s<enter>        *
 *                                                                          *
 * Last Updated: 10 Aug 2016                                                *
 *                                                                          *
@@ -72,42 +73,57 @@
 #include <stdio.h>
 #include <string.h>
 
+// size of memory operands
+typedef   signed  char      bool;
+typedef   signed  char      bit8s;
+typedef unsigned  char      bit8u;
+typedef   signed short      bit16s;
+typedef unsigned short      bit16u;
+typedef   signed  long      bit32s;
+typedef unsigned  long      bit32u;
+
+
 #pragma pack(1)
 
 #define BUF_SIZE 4096   // must be a multiple of 4 (and >= 1024)
 #define TRUE 1
 #define FALSE 0
 
+// our 32-byte header we prefix to the kernel file(s)
 struct HDR {
-  unsigned  long id;
-  unsigned  long location;
-  unsigned  long flags;
-  unsigned  long file_crc;
-  unsigned  char comp_type;
-  unsigned  char hdr_crc;
-            char resv[14];
-} hdr;
+  bit32u id;
+  bit32u location;
+  bit32u flags;
+  bit32u file_crc;
+  bit8u  comp_type;
+  bit8u  hdr_crc;
+  bit8u  resv[14];
+};
 
-unsigned int calc_crc(unsigned char *, int, unsigned int);
+// prototype
+bit32u calc_crc(bit8u *, const int, const bit32u);
 
-
+// main.
 int main(int argc, char *argv[]) {
-
   FILE *src, *targ;
-  unsigned long location = 0x00100000, org_crc = 0, *ptr;
+  struct HDR hdr;
+  bit32u location = 0x00100000, org_crc = 0, *ptr;
   size_t l;
-  unsigned char *p;
-  unsigned char crc = 0, halt = 0, ctype = 0, clear = 0, iskernel = 0;
+  bit8u *p;
+  bit8u crc = 0, ctype = 0;
+  bool halt = FALSE, clear = FALSE, iskernel = FALSE;
   int i;
   char buf[BUF_SIZE];
   char srcfile[256];
   char targfile[256];
   char orgfile[256];
   
+  // "clear out" the filenames
   strcpy(srcfile, "");
   strcpy(targfile, "");
   strcpy(orgfile, "");
   
+  // go through the command line parameters
   for (i=1; i<argc; i++) {
     if (argv[i][0] == '/') {
       if (argv[i][1] == 'e')
@@ -118,7 +134,7 @@ int main(int argc, char *argv[]) {
         iskernel = TRUE;
       else if (argv[i][1] == 'c') {
         if (isdigit(argv[i][2]))
-          ctype = (unsigned char) (argv[i][2] - '0');
+          ctype = (bit8u) (argv[i][2] - '0');
         else {
           puts(" Illegal compression type.  0-9 accepted\n");
           return -3;
@@ -154,23 +170,32 @@ int main(int argc, char *argv[]) {
     }
   }
   
+  ////////////////////////////////////////////////////////
+  // did we specify a source file name?
   if (!strlen(srcfile)) {
     puts(" Need a filename...\n");
     return -4;
   }
   
+  ////////////////////////////////////////////////////////
+  // we need a filename for the orginial file so we can 
+  // calculate the CRC
   if (!strlen(orgfile)) {
     puts(" Need the filename of the original file for crc checking...\n");
     return -4;
   }
   
+  ////////////////////////////////////////////////////////
   // get the crc of the original file
   if ((src = fopen(orgfile, "rb")) == NULL) {
     printf("\n Could not open %s", orgfile);
     return -1;
   }
-  
-  fread(buf, 1, 4, src);
+  if (fread(buf, 1, sizeof(bit32u), src) != sizeof(bit32u)) {  // get the first dword
+    printf("Could not read from %s\n", orgfile);
+    fclose(src);
+    return -5;
+  }
   org_crc  = buf[0] << 24;
   org_crc |= buf[1] << 16;
   org_crc |= buf[2] <<  8;
@@ -181,57 +206,77 @@ int main(int argc, char *argv[]) {
     //  file with the CRC check below
     if (clear) {
       org_crc = 0;
-      memset(buf, 0, 0x400 - 4);
-      clear = 0;
+      memset(buf, 0, 0x400 - 4);  // - 4 because we already read the first dword
+      clear = FALSE;
     }
     org_crc = calc_crc(buf, l, org_crc);
   } while (l == BUF_SIZE);
   fclose(src);
 
-
+  ////////////////////////////////////////////////////////
+  // open the source file
   if ((src = fopen(srcfile, "r+b")) == NULL) {
     printf("\n Could not open %s", srcfile);
     return -1;
   }
   
+  ////////////////////////////////////////////////////////
+  // create the target file
   if ((targ = fopen(targfile, "w+b")) == NULL) {
     printf("\n Could not create %s", targfile);
     return -2;
   }
   
-  memset(&hdr, 0, sizeof(hdr));
+  ////////////////////////////////////////////////////////
+  // initialize the header
+  //  and calcuate the header's crc byte
+  memset(&hdr, 0, sizeof(struct HDR));
   hdr.id = 0x46595332;
   hdr.location = location;
   hdr.flags = (((halt) ? 1 : 0) << 0) | (((iskernel) ? 1 : 0) << 1);
   hdr.file_crc = org_crc;
   hdr.comp_type = ctype;
-  p = (unsigned char *) &hdr;
-  for (i=0; i<sizeof(hdr); i++)
-    crc = (unsigned char) (crc + p[i]);
+  p = (bit8u *) &hdr;
+  for (i=0; i<sizeof(struct HDR); i++)
+    crc = (bit8u) (crc + p[i]);
   hdr.hdr_crc = -crc;
   
-  fwrite(&hdr, sizeof(hdr), 1, targ);
+  ////////////////////////////////////////////////////////
+  // write the header
+  fwrite(&hdr, 1, sizeof(struct HDR), targ);
   
+  ////////////////////////////////////////////////////////
+  // copy the remaining file
+  // (too bad there wasn't a file prefix write, huh?)
+  // (it's easy to add to the end of a file, but not the beginning...)
   do {
     l = fread(buf, 1, BUF_SIZE, src);
     fwrite(buf, 1, l, targ);
   } while (l == BUF_SIZE);
   
+  ////////////////////////////////////////////////////////
+  // close both files
   fclose(src);
   fclose(targ);
   
+  ////////////////////////////////////////////////////////
+  // delete the source and rename the target back to the source name
   remove(srcfile);
   rename(targfile, srcfile);
   
+  ////////////////////////////////////////////////////////
+  // return a successful conversion
   return 0;
 }
 
 #define QUOTIENT  0x04C11DB7
 
-unsigned int calc_crc(unsigned char *data, int len, unsigned int crc) {
-  unsigned int  result;
-  int           i, j;
-  unsigned char octet;
+////////////////////////////////////////////////////////
+// calcuate a 32-bit CRC for the file's data
+bit32u calc_crc(bit8u *data, const int len, const bit32u crc) {
+  bit32u  result;
+  int     i, j;
+  bit8u   octet;
   
   result = ~crc;
   for (i=0; i<len; i++) {
