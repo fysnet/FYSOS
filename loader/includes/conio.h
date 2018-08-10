af649693f01a0f64441959a56c5257ce31347996
Benjamin David Lunt <spamcollector@cableone.net>
2018-08-10 16:34:48 -0700

    Update Copyright and other Info

--- loader/includes/conio.h
+++ loader/includes/conio.h
@@ -1,3 +1,26 @@
+/*             Author: Benjamin David Lunt
+ *                     Forever Young Software
+ *                     Copyright (c) 1984-2018
+ *  
+ *  This code is donated to the Freeware communitee.  You have the
+ *   right to use it for learning purposes only.  You may not modify it
+ *   for redistribution for any other purpose unless you have written
+ *   permission from the author.
+ *
+ *  You may modify and use it in your own projects as long as they are
+ *   for non-profit only and not distributed.  Any project for profit that 
+ *   uses this code must have written permission from the author.
+ *
+ *  For more information:
+ *    http://www.fysnet.net/osdesign_book_series.htm
+ *  Contact:
+ *    fys [at] fysnet [dot] net
+ *
+ * Last update:  10 Aug 2018
+ *
+ * compile using SmallerC  (https://github.com/alexfru/SmallerC/)
+ *  smlrcc @make.txt
+ */
 #ifndef CONIO_H
 #define CONIO_H
 
