c094a86e761e1163a15ab5bf735868baaa633126
Benjamin David Lunt <spamcollector@cableone.net>
2020-07-11 20:21:21 -0700

    Create mimage.h

---
+++ main/syscore/utils/mimage/mimage.h
@@ -0,0 +1,21 @@
+/*             Author: Benjamin David Lunt
+ *                     Forever Young Software
+ *                     Copyright (c) 1984-2015
+ *  
+ *  This code is included on the disc that is included with the book
+ *   FYSOS: The System Core, and is for that purpose only.  You have the
+ *   right to use it for learning purposes only.  You may not modify it for
+ *   redistribution for any other purpose unless you have written permission
+ *   from the author.
+ *
+ *  You may modify and use it in your own projects as long as they are
+ *   for non profit only and not distributed.  Any project for profit that 
+ *   uses this code must have written permission from the author.
+ */
+
+// set it to 1 (align on byte)
+#pragma pack (1)
+
+char strtstr[] = "\nMIMAGE  v1.00.00    Forever Young Software 1984-2015";
+
+void parse_command(int, char *[], char *, bit32u *);
