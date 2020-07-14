8489bff94cf1240400c6c63b34039ed649b0ae39
Benjamin David Lunt <spamcollector@cableone.net>
2020-07-13 20:36:17 -0700

    Update license

--- main/usb/utils/detcntlr/detcntlr.h
+++ main/usb/utils/detcntlr/detcntlr.h
@@ -5,19 +5,22 @@
  *                            fys [at] fysnet [dot] net
  *                              All rights reserved
  * 
- * Redistribution and use in source and binary forms with or without modification,
- * are permitted provided that the following conditions are met:
+ * Redistribution and use in source or binary forms with  or without modification,
+ * are  permitted provided that the  following conditions are met.  Redistribution
+ * in printed form must first acquire permission from copyright holder.
  * 
  * 1. Redistributions of source  code must retain the above copyright notice, this
  *    list of conditions and the following disclaimer.
  * 2. Redistributions in  binary form must  reproduce the above copyright  notice,
  *    this list of  conditions and the following  disclaimer in the  documentation
  *    and/or other materials provided with the distribution.
+ * 3. Redistributions in printed form must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
  * 
- * THIS SOFTWARE IS PROVIDED BY  THE COPYRIGHT HOLDER AND  CONTRIBUTOR "AS IS" AND
+ * THIS SOFTWARE IS  PROVIDED BY THE COPYRIGHT  HOLDER AND CONTRIBUTOR "AS IS" AND
  * ANY EXPRESS OR IMPLIED  WARRANTIES, INCLUDING, BUT NOT  LIMITED TO, THE IMPLIED
  * WARRANTIES  OF  MERCHANTABILITY  AND  FITNESS  FOR  A  PARTICULAR  PURPOSE  ARE 
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTOR  BE LIABLE FOR
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT  OWNER OR CONTRIBUTOR BE LIABLE FOR
  * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,  OR CONSEQUENTIAL DAMAGES
  * (INCLUDING, BUT NOT LIMITED TO,  PROCUREMENT OF  SUBSTITUTE GOODS  OR SERVICES;
  * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER  CAUSED AND ON
@@ -39,13 +42,12 @@
  *    Volume 7:  ** Not yet published **
  *    Volume 8:  USB: The Universal Serial Bus
  * 
- * This product is  included as a companion  to one or more of these  books and is
- * not intended to be self-sufficient.  Each item within this distribution is part
- * of a discussion within one or more of the books mentioned above and is released
- * soley for that purpose.
+ * This product here is  included as a companion to one or more of these books and 
+ * is not intended to be  self sufficient.  Each item within  this distribution is
+ * part of a discussion within one or more of the books mentioned above.
  * 
  * For more information, please visit:
- * http://www.fysnet.net/osdesign_book_series.htm
+ *             http://www.fysnet.net/osdesign_book_series.htm
  */
 
 #ifndef FYSOS_DETCNTLR
