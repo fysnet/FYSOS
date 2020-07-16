088beed57be6925d4b9bde6af250bea63276e5d8
Benjamin David Lunt <spamcollector@cableone.net>
2020-07-15 18:15:24 -0700

    Update for new code release

--- main/filesys/source/cd_menu/cd_menu.asm
+++ main/filesys/source/cd_menu/cd_menu.asm
@@ -1,47 +1,83 @@
-comment /******************************************************************\
-*                            FYS OS version 2.0                            *
-*                         Forever Young Software                           *
-*                        Copyright (c) 1984-2019                           *
-*                                                                          *
-* FILE: cd_menu.asm                                                        *
-*   *** Assumes this is for a floppy drive ***                             *
-*                                                                          *
-* This code is freeware, not public domain.  Please use respectfully.      *
-*                                                                          *
-* Multi-boot CD-ROM menu system for an 'El Torito' formatted bootable      *
-*  CD-ROM.  This code gets placed at 0x07C00 by the BIOS.  It loads all    *
-*  the sectors of the file for us.                                         *
-*                                                                          *
-*  Built with:  NBASM ver 00.26.xx                                         *
-*                 http:\\www.fysnet.net\newbasic.htm                       *
-*                                                                          *
-* Last Update:  22 Sept 2018                                               *
-*                                                                          *
-*                                                                          *
-****************************************************************************
-* Assuptions:                                                              *
-* - We assume we are at least a 80x386 machine.                            *
-*                                                                          *
-*                                                                          *
-*                                                                          *
-****************************************************************************
-* Notes:                                                                   *
-* - This 'El Torito' BIOS loads us to 0x07C0:0000, all sectors of the file.*
-*   Once the BIOS jumps to 0x07C0:0000, the program can retrieve its boot  *
-*   information by issuing INT 13, Function 4B, AL=01. After the boot      *
-*   process has been initiated the INT 13 Extensions (functions 41-48)     *
-*   will access the CD using 0800h byte sectors and the LBA address        *
-*   provided to INT 13 is an absolute sector number.                       *
-*                                                                          *
-*                                                                          *
-*                                                                          *
-*                                                                          *
-*                                                                          *
-* Hardware Requirements:                                                   *
-*  This code uses instructions that are valid for a 186 or later Intel x86 *
-*  or compatible CPU.                                                      *
-*                                                                          *
-***************************************************************************/
+ ;
+ ;                             Copyright (c) 1984-2020
+ ;                              Benjamin David Lunt
+ ;                             Forever Young Software
+ ;                            fys [at] fysnet [dot] net
+ ;                              All rights reserved
+ ; 
+ ; Redistribution and use in source or resulting in  compiled binary forms with or
+ ; without modification, are permitted provided that the  following conditions are
+ ; met.  Redistribution in printed form must first acquire written permission from
+ ; copyright holder.
+ ; 
+ ; 1. Redistributions of source  code must retain the above copyright notice, this
+ ;    list of conditions and the following disclaimer.
+ ; 2. Redistributions in printed form must retain the above copyright notice, this
+ ;    list of conditions and the following disclaimer.
+ ; 3. Redistributions in  binary form must  reproduce the above copyright  notice,
+ ;    this list of  conditions and the following  disclaimer in the  documentation
+ ;    and/or other materials provided with the distribution.
+ ; 
+ ; THIS SOFTWARE, DOCUMENTATION, BINARY FILES, OR OTHER ITEM, HEREBY FURTHER KNOWN
+ ; AS 'PRODUCT', IS  PROVIDED BY THE COPYRIGHT  HOLDER AND CONTRIBUTOR "AS IS" AND
+ ; ANY EXPRESS OR IMPLIED  WARRANTIES, INCLUDING, BUT NOT  LIMITED TO, THE IMPLIED
+ ; WARRANTIES  OF  MERCHANTABILITY  AND  FITNESS  FOR  A  PARTICULAR  PURPOSE  ARE 
+ ; DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT  OWNER OR CONTRIBUTOR BE LIABLE FOR
+ ; ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,  OR CONSEQUENTIAL DAMAGES
+ ; (INCLUDING, BUT NOT LIMITED TO,  PROCUREMENT OF  SUBSTITUTE GOODS  OR SERVICES;
+ ; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER  CAUSED AND ON
+ ; ANY  THEORY OF  LIABILITY, WHETHER  IN  CONTRACT,  STRICT  LIABILITY,  OR  TORT 
+ ; (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN  ANY WAY  OUT OF THE USE OF THIS
+ ; PRODUCT, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  READER AND/OR USER
+ ; USES AS THEIR OWN RISK.
+ ; 
+ ; Any inaccuracy in source code, code comments, documentation, or other expressed
+ ; form within Product,  is unintentional and corresponding hardware specification
+ ; takes precedence.
+ ; 
+ ; Let it be known that  the purpose of this Product is to be used as supplemental
+ ; product for one or more of the following mentioned books.
+ ; 
+ ;   FYSOS: Operating System Design
+ ;    Volume 1:  The System Core
+ ;    Volume 2:  The Virtual File System
+ ;    Volume 3:  Media Storage Devices
+ ;    Volume 4:  Input and Output Devices
+ ;    Volume 5:  ** Not yet published **
+ ;    Volume 6:  The Graphical User Interface
+ ;    Volume 7:  ** Not yet published **
+ ;    Volume 8:  USB: The Universal Serial Bus
+ ; 
+ ; This Product is  included as a companion  to one or more of these  books and is
+ ; not intended to be self-sufficient.  Each item within this distribution is part
+ ; of a discussion within one or more of the books mentioned above.
+ ; 
+ ; For more information, please visit:
+ ;             http://www.fysnet.net/osdesign_book_series.htm
+ 
+
+ ;
+ ;  CD_MENU.ASM
+ ;   Multi-boot CD-ROM menu system for an 'El Torito' formatted bootable
+ ;   CD-ROM.  This code gets placed at 0x07C00 by the BIOS.  It loads all
+ ;   the sectors of the file for us.
+ ;  
+ ;  Hardware Requirements:
+ ;    This code uses instructions that are valid for a 386 or later 
+ ;    Intel x86 or compatible CPU.
+ ;
+ ;  Last updated: 15 July 2020
+ ;
+ ;  Assembled using (NBASM v00.26.74) (http://www.fysnet/newbasic.htm)
+ ;   nbasm usbboot
+ ;
+ ;  Notes:
+ ;   This 'El Torito' BIOS loads us to 0x07C0:0000, all sectors of the file.
+ ;   Once the BIOS jumps to 0x07C0:0000, the program can retrieve its boot
+ ;   information by issuing INT 13, Function 4B, AL=01. After the boot
+ ;   process has been initiated the INT 13 Extensions (functions 41-48)
+ ;   will access the CD using 0800h byte sectors and the LBA address
+ ;   provided to INT 13 is an absolute sector number.
 
 .model tiny                        ;
 
