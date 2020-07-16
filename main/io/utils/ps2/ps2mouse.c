bec14cff5af04e694416c7cd6d0b1babb91eef08
Benjamin David Lunt <spamcollector@cableone.net>
2020-07-15 19:11:33 -0700

    Update for new code release

--- main/io/utils/ps2/ps2mouse.c
+++ main/io/utils/ps2/ps2mouse.c
@@ -114,7 +114,7 @@ int main(int argc, char *argv[]) {
   bool mode = KEY_MODE_PS2;
   
   printf("Detect PS/2 Controller and Mouse.      v1.00.00\n"
-         "Forever Young Software  --  Copyright 1984-2016\n\n");
+         "Forever Young Software  --  Copyright 1984-2020\n\n");
   
   // for slow machines, don't count so high
   // for fast machines, delay needs to be larger counter
