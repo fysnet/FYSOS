395a48633852a34ab42cf1944af03c65ee189530
Benjamin David Lunt <spamcollector@cableone.net>
2020-07-19 19:29:14 -0700

    updated code for new release

--- main/syscore/source/fdc_boot/fdc_boot.asm
+++ main/syscore/source/fdc_boot/fdc_boot.asm
@@ -1,71 +1,100 @@
-comment |*******************************************************************
-*  Copyright (c) 1984-2019    Forever Young Software  Benjamin David Lunt  *
-*                                                                          *
-*                            FYS OS version 2.0                            *
-* FILE: fdc_boot.asm                                                       *
-*                                                                          *
-* This code is freeware, not public domain.  Please use respectfully.      *
-*                                                                          *
-* You may:                                                                 *
-*  - use this code for learning purposes only.                             *
-*  - use this code in your own Operating System development.               *
-*  - distribute any code that you produce pertaining to this code          *
-*    as long as it is for learning purposes only, not for profit,          *
-*    and you give credit where credit is due.                              *
-*                                                                          *
-* You may NOT:                                                             *
-*  - distribute this code for any purpose other than listed above.         *
-*  - distribute this code for profit.                                      *
-*                                                                          *
-* You MUST:                                                                *
-*  - include this whole comment block at the top of this file.             *
-*  - include contact information to where the original source is located.  *
-*            https://github.com/fysnet/FYSOS                               *
-*                                                                          *
-* DESCRIPTION:                                                             *
-*   To see if I can read remaining sectors from a booted floppy disk       *
-*    without using the BIOS.  I.e.: Directly program the FDC to read       *
-*    from the disk.                                                        *
-*   This must be all coded in a single sector since we cannot load any     *
-*    addition sectors using the BIOS.  All read code, FDC, DMA (if used),  *
-*    and other code must fit in the first sector already loaded for us     *
-*    by the BIOS.                                                          *
-*   This is simply an experiment to see if it can be done somewhat         *
-*    reliably.  i.e.: very few assumptions, and has error checking.        *
-*   I just wanted to do it for fun.                                        *
-*   It only reads in the next sector and jumps to it.  However, it should  *
-*    be quite simple to add additional sectors, different starting sector, *
-*    etc.                                                                  *
-*                                                                          *
-* BUILT WITH:   NewBasic Assembler                                         *
-*                 http://www.fysnet/newbasic.htm                           *
-*               NBASM ver 00.26.76                                         *
-*          Command line: nbasm fdc_boot<enter>                             *
-*                                                                          *
-* Last Updated: 7 Dec 2019                                                 *
-*                                                                          *
-****************************************************************************
-* Notes:                                                                   *
-*                                                                          *
-*  Assuptions:                                                             *
-*   - the disk is a 1.44meg, 3 1/2" standard formatted floppy disk.        *
-*   - this will be the first drive of the FDC.  i.e.: Drv = DRIVE          *
-*     (we could get the DL value from the POST and use that to select      *
-*      the drive.  i.e.:  DL = 00h = first drive, DL = 01h = second drive) *
-*   - this will be at either 0x3F0 or 0x370. i.e.: controler = CNTRLR      *
-*   - the disk is already inserted.  It pretty much has to be since the    *
-*      BIOS just read this sector.                                         *
-*                                                                          *
-* Hardware Requirements:                                                   *
-*  This code uses commands that are valid for a 186 or later Intel x86     *
-*  or compatible CPU.                                                      *
-*                                                                          *
-* This code uses some size optimizations here and there to get it to fit   *
-*   with in the 512 byte sector(s).  Be careful when assuming the          *
-*   optimizations work if you modify the code.  i.e.: previous register    *
-*   values, etc.                                                           *
-*                                                                          *
-***************************************************************************|
+ ;
+ ;                             Copyright (c) 1984-2020
+ ;                              Benjamin David Lunt
+ ;                             Forever Young Software
+ ;                            fys [at] fysnet [dot] net
+ ;                              All rights reserved
+ ; 
+ ; Redistribution and use in source or resulting in  compiled binary forms with or
+ ; without modification, are permitted provided that the  following conditions are
+ ; met.  Redistribution in printed form must first acquire written permission from
+ ; copyright holder.
+ ; 
+ ; 1. Redistributions of source  code must retain the above copyright notice, this
+ ;    list of conditions and the following disclaimer.
+ ; 2. Redistributions in printed form must retain the above copyright notice, this
+ ;    list of conditions and the following disclaimer.
+ ; 3. Redistributions in  binary form must  reproduce the above copyright  notice,
+ ;    this list of  conditions and the following  disclaimer in the  documentation
+ ;    and/or other materials provided with the distribution.
+ ; 
+ ; THIS SOFTWARE, DOCUMENTATION, BINARY FILES, OR OTHER ITEM, HEREBY FURTHER KNOWN
+ ; AS 'PRODUCT', IS  PROVIDED BY THE COPYRIGHT  HOLDER AND CONTRIBUTOR "AS IS" AND
+ ; ANY EXPRESS OR IMPLIED  WARRANTIES, INCLUDING, BUT NOT  LIMITED TO, THE IMPLIED
+ ; WARRANTIES  OF  MERCHANTABILITY  AND  FITNESS  FOR  A  PARTICULAR  PURPOSE  ARE 
+ ; DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT  OWNER OR CONTRIBUTOR BE LIABLE FOR
+ ; ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,  OR CONSEQUENTIAL DAMAGES
+ ; (INCLUDING, BUT NOT LIMITED TO,  PROCUREMENT OF  SUBSTITUTE GOODS  OR SERVICES;
+ ; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER  CAUSED AND ON
+ ; ANY  THEORY OF  LIABILITY, WHETHER  IN  CONTRACT,  STRICT  LIABILITY,  OR  TORT 
+ ; (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN  ANY WAY  OUT OF THE USE OF THIS
+ ; PRODUCT, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  READER AND/OR USER
+ ; USES AS THEIR OWN RISK.
+ ; 
+ ; Any inaccuracy in source code, code comments, documentation, or other expressed
+ ; form within Product,  is unintentional and corresponding hardware specification
+ ; takes precedence.
+ ; 
+ ; Let it be known that  the purpose of this Product is to be used as supplemental
+ ; product for one or more of the following mentioned books.
+ ; 
+ ;   FYSOS: Operating System Design
+ ;    Volume 1:  The System Core
+ ;    Volume 2:  The Virtual File System
+ ;    Volume 3:  Media Storage Devices
+ ;    Volume 4:  Input and Output Devices
+ ;    Volume 5:  ** Not yet published **
+ ;    Volume 6:  The Graphical User Interface
+ ;    Volume 7:  ** Not yet published **
+ ;    Volume 8:  USB: The Universal Serial Bus
+ ; 
+ ; This Product is  included as a companion  to one or more of these  books and is
+ ; not intended to be self-sufficient.  Each item within this distribution is part
+ ; of a discussion within one or more of the books mentioned above.
+ ; 
+ ; For more information, please visit:
+ ;             http://www.fysnet.net/osdesign_book_series.htm
+ 
+ ;
+ ;  FDC_BOOT.ASM
+ ;   To see if I can read remaining sectors from a booted floppy disk
+ ;    without using the BIOS.  I.e.: Directly program the FDC to read
+ ;    from the disk.
+ ;   This must be all coded in a single sector since we cannot load any
+ ;    addition sectors using the BIOS.  All read code, FDC, DMA (if used),
+ ;    and other code must fit in the first sector already loaded for us
+ ;    by the BIOS.
+ ;   This is simply an experiment to see if it can be done somewhat
+ ;    reliably.  i.e.: very few assumptions, and has error checking.
+ ;   I just wanted to do it for fun.
+ ;   It only reads in the next sector and jumps to it.  However, it should
+ ;    be quite simple to add additional sectors, different starting sector,
+ ;    etc.
+ ;
+ ;  Last updated: 19 July 2020
+ ;
+ ;  Assembled using (NBASM v00.26.74) (http://www.fysnet/newbasic.htm)
+ ;   nbasm usbboot
+ ;
+
+ ; Notes:
+ ;  Assuptions:
+ ;   - the disk is a 1.44meg, 3 1/2" standard formatted floppy disk.
+ ;   - this will be the first drive of the FDC.  i.e.: Drv = DRIVE
+ ;     (we could get the DL value from the POST and use that to select
+ ;      the drive.  i.e.:  DL = 00h = first drive, DL = 01h = second drive)
+ ;   - this will be at either 0x3F0 or 0x370. i.e.: controler = CNTRLR
+ ;   - the disk is already inserted.  It pretty much has to be since the
+ ;      BIOS just read this sector.
+ ;
+ ; Hardware Requirements:
+ ;  This code uses commands that are valid for a 186 or later Intel x86
+ ;  or compatible CPU.
+ ;
+ ; This code uses some size optimizations here and there to get it to fit
+ ;   with in the 512 byte sector(s).  Be careful when assuming the
+ ;   optimizations work if you modify the code.  i.e.: previous register
+ ;   values, etc.
 
 .model tiny
 
