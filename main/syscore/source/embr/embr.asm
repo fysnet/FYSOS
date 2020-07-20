9a34ae849e5143bdfe005032ae5b26a432933d0f
Benjamin David Lunt <spamcollector@cableone.net>
2020-07-19 19:24:08 -0700

    updated code for new release

--- main/syscore/source/embr/embr.asm
+++ main/syscore/source/embr/embr.asm
@@ -1,63 +1,98 @@
-comment |*******************************************************************
-*  Copyright (c) 1984-2017    Forever Young Software  Benjamin David Lunt  *
-*                                                                          *
-*                            FYS OS version 2.0                            *
-* FILE: embr.asm                                                           *
-*                                                                          *
-* This code is freeware, not public domain.  Please use respectfully.      *
-*                                                                          *
-* You may:                                                                 *
-*  - use this code for learning purposes only.                             *
-*  - use this code in your own Operating System development.               *
-*  - distribute any code that you produce pertaining to this code          *
-*    as long as it is for learning purposes only, not for profit,          *
-*    and you give credit where credit is due.                              *
-*                                                                          *
-* You may NOT:                                                             *
-*  - distribute this code for any purpose other than listed above.         *
-*  - distribute this code for profit.                                      *
-*                                                                          *
-* You MUST:                                                                *
-*  - include this whole comment block at the top of this file.             *
-*  - include contact information to where the original source is located.  *
-*            https://github.com/fysnet/FYSOS                               *
-*                                                                          *
-* DESCRIPTION:                                                             *
-*   embr boot code                                                         *
-*                                                                          *
-* BUILT WITH:   NewBasic Assembler                                         *
-*                 http://www.fysnet/newbasic.htm                           *
-*               NBASM ver 00.26.59                                         *
-*          Command line: nbasm embr -d<enter>                              *
-*                                                                          *
-* Last Updated: 29 May 2017                                                *
-*                                                                          *
-****************************************************************************
-* Notes:                                                                   *
-*                                                                          *
-* This code and the eMBR technique is copyrighted.  You may use this       *
-*  and the eMBR technique as you would like as long as you do not change   *
-*  the overall function of the use and you must also include the following *
-*  copyright line in all code and documentation pertaining to the use      *
-*  of this technique.                                                      *
-*                                                                          *
-* This code must be assembled with NBASM version 00.26.31 or later.  Any   *
-*  version before this will not correctly assemble the 64-bit immediate    *
-*  values with the DQ declaration.  Versions before also had a bug in the  *
-*  IMUL reg32,reg3,immed instruction.                                      *
-*                                                                          *
-* This code reads in any remaining code and all of the EMBR entries. Once  *
-*  a partition is selected, it is read in to a buffer location, then       *
-*  copied to 0x07C00.  We use this technique so that we don't have to move *
-*  out of the way.                                                         *
-*                                                                          *
-* This code assumes that it is on LBA 1.  You should have a valid MBR at   *
-*  LBA 0. If you modify this code and re-build it, make sure you place     *
-*  a valid MBR at LBA 0 and this code at LBA 1.                            *
-*                                                                          *
-* This code also assumes you will not be on a floppy disk.                 *
-*                                                                          *
-***************************************************************************|
+ ;
+ ;                             Copyright (c) 1984-2020
+ ;                              Benjamin David Lunt
+ ;                             Forever Young Software
+ ;                            fys [at] fysnet [dot] net
+ ;                              All rights reserved
+ ; 
+ ; Redistribution and use in source or resulting in  compiled binary forms with or
+ ; without modification, are permitted provided that the  following conditions are
+ ; met.  Redistribution in printed form must first acquire written permission from
+ ; copyright holder.
+ ; 
+ ; 1. Redistributions of source  code must retain the above copyright notice, this
+ ;    list of conditions and the following disclaimer.
+ ; 2. Redistributions in printed form must retain the above copyright notice, this
+ ;    list of conditions and the following disclaimer.
+ ; 3. Redistributions in  binary form must  reproduce the above copyright  notice,
+ ;    this list of  conditions and the following  disclaimer in the  documentation
+ ;    and/or other materials provided with the distribution.
+ ; 
+ ; THIS SOFTWARE, DOCUMENTATION, BINARY FILES, OR OTHER ITEM, HEREBY FURTHER KNOWN
+ ; AS 'PRODUCT', IS  PROVIDED BY THE COPYRIGHT  HOLDER AND CONTRIBUTOR "AS IS" AND
+ ; ANY EXPRESS OR IMPLIED  WARRANTIES, INCLUDING, BUT NOT  LIMITED TO, THE IMPLIED
+ ; WARRANTIES  OF  MERCHANTABILITY  AND  FITNESS  FOR  A  PARTICULAR  PURPOSE  ARE 
+ ; DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT  OWNER OR CONTRIBUTOR BE LIABLE FOR
+ ; ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,  OR CONSEQUENTIAL DAMAGES
+ ; (INCLUDING, BUT NOT LIMITED TO,  PROCUREMENT OF  SUBSTITUTE GOODS  OR SERVICES;
+ ; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER  CAUSED AND ON
+ ; ANY  THEORY OF  LIABILITY, WHETHER  IN  CONTRACT,  STRICT  LIABILITY,  OR  TORT 
+ ; (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN  ANY WAY  OUT OF THE USE OF THIS
+ ; PRODUCT, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  READER AND/OR USER
+ ; USES AS THEIR OWN RISK.
+ ; 
+ ; Any inaccuracy in source code, code comments, documentation, or other expressed
+ ; form within Product,  is unintentional and corresponding hardware specification
+ ; takes precedence.
+ ; 
+ ; Let it be known that  the purpose of this Product is to be used as supplemental
+ ; product for one or more of the following mentioned books.
+ ; 
+ ;   FYSOS: Operating System Design
+ ;    Volume 1:  The System Core
+ ;    Volume 2:  The Virtual File System
+ ;    Volume 3:  Media Storage Devices
+ ;    Volume 4:  Input and Output Devices
+ ;    Volume 5:  ** Not yet published **
+ ;    Volume 6:  The Graphical User Interface
+ ;    Volume 7:  ** Not yet published **
+ ;    Volume 8:  USB: The Universal Serial Bus
+ ; 
+ ; This Product is  included as a companion  to one or more of these  books and is
+ ; not intended to be self-sufficient.  Each item within this distribution is part
+ ; of a discussion within one or more of the books mentioned above.
+ ; 
+ ; For more information, please visit:
+ ;             http://www.fysnet.net/osdesign_book_series.htm
+ 
+ ;
+ ;  EMBR.BIN
+ ;   EMBR boot code.  This is to show how to load and display partitions
+ ;    using the EMBR partitioning scheme.
+ ;   
+ ;   Hardware Requirements:
+ ;    This code uses instructions that are valid for a 386 or later 
+ ;    Intel x86 or compatible CPU.
+ ;
+ ;  Last updated: 19 July 2020
+ ;
+ ;  Assembled using (NBASM v00.26.74) (http://www.fysnet/newbasic.htm)
+ ;   nbasm embr
+ ;
+ 
+ ; Notes:
+ ;
+ ; This code and the eMBR technique is copyrighted.  You may use this
+ ;  and the eMBR technique as you would like as long as you do not change
+ ;  the overall function of the use and you must also include the following
+ ;  copyright line in all code and documentation pertaining to the use
+ ;  of this technique.
+ ;
+ ; This code must be assembled with NBASM version 00.26.31 or later.  Any
+ ;  version before this will not correctly assemble the 64-bit immediate
+ ;  values with the DQ declaration.  Versions before also had a bug in the
+ ;  IMUL reg32,reg3,immed instruction.
+ ;
+ ; This code reads in any remaining code and all of the EMBR entries. Once
+ ;  a partition is selected, it is read in to a buffer location, then
+ ;  copied to 0x07C00.  We use this technique so that we don't have to move
+ ;  out of the way.
+ ;
+ ; This code assumes that it is on LBA 1.  You should have a valid MBR at
+ ;  LBA 0. If you modify this code and re-build it, make sure you place
+ ;  a valid MBR at LBA 0 and this code at LBA 1.
+ ;
+ ; This code also assumes you will not be on a floppy disk.
 
 .model tiny                        ;
 .diag 0
