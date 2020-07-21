4f2f3582dcea27b70177d9a86a27ae7fa8cbc22e
Benjamin David Lunt <spamcollector@cableone.net>
2020-07-20 17:25:05 -0700

    updated code for new release

--- main/syscore/utils/mps/mps.c
+++ main/syscore/utils/mps/mps.c
@@ -1,27 +1,75 @@
-/*             Author: Benjamin David Lunt
- *                     Forever Young Software
- *                     Copyright (c) 1984-2017
- *  
- *  This code is included on the disc that is included with the book
- *   FYSOS: The System Core, and is for that purpose only.  You have the
- *   right to use it for learning purposes only.  You may not modify it for
- *   redistribution for any other purpose unless you have written permission
- *   from the author.
- *
- *  You may modify and use it in your own projects as long as they are
- *   for non profit only and not distributed.  Any project for profit that 
- *   uses this code must have written permission from the author.
+/*
+ *                             Copyright (c) 1984-2020
+ *                              Benjamin David Lunt
+ *                             Forever Young Software
+ *                            fys [at] fysnet [dot] net
+ *                              All rights reserved
+ * 
+ * Redistribution and use in source or resulting in  compiled binary forms with or
+ * without modification, are permitted provided that the  following conditions are
+ * met.  Redistribution in printed form must first acquire written permission from
+ * copyright holder.
+ * 
+ * 1. Redistributions of source  code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in printed form must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 3. Redistributions in  binary form must  reproduce the above copyright  notice,
+ *    this list of  conditions and the following  disclaimer in the  documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE, DOCUMENTATION, BINARY FILES, OR OTHER ITEM, HEREBY FURTHER KNOWN
+ * AS 'PRODUCT', IS  PROVIDED BY THE COPYRIGHT  HOLDER AND CONTRIBUTOR "AS IS" AND
+ * ANY EXPRESS OR IMPLIED  WARRANTIES, INCLUDING, BUT NOT  LIMITED TO, THE IMPLIED
+ * WARRANTIES  OF  MERCHANTABILITY  AND  FITNESS  FOR  A  PARTICULAR  PURPOSE  ARE 
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT  OWNER OR CONTRIBUTOR BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,  OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO,  PROCUREMENT OF  SUBSTITUTE GOODS  OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER  CAUSED AND ON
+ * ANY  THEORY OF  LIABILITY, WHETHER  IN  CONTRACT,  STRICT  LIABILITY,  OR  TORT 
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN  ANY WAY  OUT OF THE USE OF THIS
+ * PRODUCT, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  READER AND/OR USER
+ * USES AS THEIR OWN RISK.
+ * 
+ * Any inaccuracy in source code, code comments, documentation, or other expressed
+ * form within Product,  is unintentional and corresponding hardware specification
+ * takes precedence.
+ * 
+ * Let it be known that  the purpose of this Product is to be used as supplemental
+ * product for one or more of the following mentioned books.
+ * 
+ *   FYSOS: Operating System Design
+ *    Volume 1:  The System Core
+ *    Volume 2:  The Virtual File System
+ *    Volume 3:  Media Storage Devices
+ *    Volume 4:  Input and Output Devices
+ *    Volume 5:  ** Not yet published **
+ *    Volume 6:  The Graphical User Interface
+ *    Volume 7:  ** Not yet published **
+ *    Volume 8:  USB: The Universal Serial Bus
+ * 
+ * This Product is  included as a companion  to one or more of these  books and is
+ * not intended to be self-sufficient.  Each item within this distribution is part
+ * of a discussion within one or more of the books mentioned above.
+ * 
+ * For more information, please visit:
+ *             http://www.fysnet.net/osdesign_book_series.htm
  */
 
-/* This utility is compiled as a 16-bit DOS .EXE using a 16-bit compiler.
- * This utility uses 16-bit segmented far pointers where a far pointer is
- *  made up as a 32-bit address of two 16-bit values:  seg:offset
+/*
+ *  MPS.EXE
  *
- * To use, simply type MPS at the DOS prompt, or redirect the output
- *  to a text file with:
+ *  Assumptions/prerequisites:
  *
- *   C:\>MPS > results.txt
+ *  Last updated: 20 July 2020
  *
+ *  This utility is compiled as a 16-bit DOS .EXE using a 16-bit compiler.
+ *  This utility uses 16-bit segmented far pointers where a far pointer is
+ *   made up as a 32-bit address of two 16-bit values:  seg:offset
+ *
+ *  Usage:
+ *   MPS
+ *   MPS > results.txt
  */
 
 #include <conio.h>
@@ -47,7 +95,7 @@ int main(int argc, char *argv[]) {
   int i;
   
   // print start string
-	printf("\nDump MPS  v00.10.00 (C)opyright   Forever Young Software 1984-2017\n\n");
+  printf("\nDump MPS  v00.10.00 (C)opyright   Forever Young Software 1984-2020\n\n");
   
   // first get the EBDA address from the BIOS
   // the BIOS should give us a 16-bit segment at 0x0040E
