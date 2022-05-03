f4c49647bc185249119a51c556a17f9bdde3f50c
Benjamin David Lunt <spamcollector@cableone.net>
2022-05-02 18:58:33 -0700

    flag name change

--- bucket/bucket.cpp
+++ bucket/bucket.cpp
@@ -27,15 +27,15 @@
 #define MODNAME "bucket.cpp"
 
 
-#define MALLOC_FLAGS_NONE       (1 << 0)   // any memory, physical or linear, non contiguous, any location
-#define MALLOC_FLAGS_CLEAR      (1 << 1)   // clear the memory on return
-#define MALLOC_FLAGS_LOW1MEG    (1 << 2)   // must be before the 1 Meg mark, inclusive
-#define MALLOC_FLAGS_LOW16MEG   (1 << 3)   // must be before the 16 Meg mark, inclusive
-#define MALLOC_FLAGS_LOW4GIG    (1 << 4)   // must be before the 4 Gig mark, inclusive
-#define MALLOC_FLAGS_PHYSICAL   (1 << 5)   // must be physical address and contiguous, inclusive
+#define MALLOC_FLAGS_VIRTUAL    (1 << 0)   // any memory, physical or linear, non contiguous, any location
+#define MALLOC_FLAGS_PHYSICAL   (1 << 1)   // must be physical address and contiguous, inclusive
+#define MALLOC_FLAGS_CLEAR      (1 << 2)   // clear the memory on return
+#define MALLOC_FLAGS_LOW1MEG    (1 << 3)   // must be before the 1 Meg mark, inclusive
+#define MALLOC_FLAGS_LOW16MEG   (1 << 4)   // must be before the 16 Meg mark, inclusive
+#define MALLOC_FLAGS_LOW4GIG    (1 << 5)   // must be before the 4 Gig mark, inclusive
 #define MALLOC_FLAGS_ALIGNED    (1 << 6)   // must be aligned.  The aligned parameter is now used, else it is ignored.
-#define MALLOC_HARDWARE32       (MALLOC_FLAGS_LOW4GIG | MALLOC_FLAGS_PHYSICAL | MALLOC_FLAGS_CLEAR)
-#define MALLOC_HARDWARE64       (                       MALLOC_FLAGS_PHYSICAL | MALLOC_FLAGS_CLEAR)
+define MALLOC_HARDWARE32       (MALLOC_FLAGS_LOW4GIG | MALLOC_FLAGS_PHYSICAL | MALLOC_FLAGS_CLEAR)
+#define MALLOC_HARDWARE64      (                       MALLOC_FLAGS_PHYSICAL | MALLOC_FLAGS_CLEAR)
 
 
 // kernel's memory heap
