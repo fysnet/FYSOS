359da2f403e8f24c4f94dd775223212c97ccd6c0
Benjamin David Lunt <spamcollector@cableone.net>
2022-11-15 13:21:54 -0700

    Update mleanfs to match new specs

--- main/filesys/utils/mleanfs/mleanfs.h
+++ main/filesys/utils/mleanfs/mleanfs.h
@@ -4,12 +4,12 @@
  *                             Forever Young Software
  *                            fys [at] fysnet [dot] net
  *                              All rights reserved
- * 
+ *
  * Redistribution and use in source or resulting in  compiled binary forms with or
  * without modification, are permitted provided that the  following conditions are
  * met.  Redistribution in printed form must first acquire written permission from
  * copyright holder.
- * 
+ *
  * 1. Redistributions of source  code must retain the above copyright notice, this
  *    list of conditions and the following disclaimer.
  * 2. Redistributions in printed form must retain the above copyright notice, this
@@ -17,27 +17,27 @@
  * 3. Redistributions in  binary form must  reproduce the above copyright  notice,
  *    this list of  conditions and the following  disclaimer in the  documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE, DOCUMENTATION, BINARY FILES, OR OTHER ITEM, HEREBY FURTHER KNOWN
  * AS 'PRODUCT', IS  PROVIDED BY THE COPYRIGHT  HOLDER AND CONTRIBUTOR "AS IS" AND
  * ANY EXPRESS OR IMPLIED  WARRANTIES, INCLUDING, BUT NOT  LIMITED TO, THE IMPLIED
- * WARRANTIES  OF  MERCHANTABILITY  AND  FITNESS  FOR  A  PARTICULAR  PURPOSE  ARE 
+ * WARRANTIES  OF  MERCHANTABILITY  AND  FITNESS  FOR  A  PARTICULAR  PURPOSE  ARE
  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT  OWNER OR CONTRIBUTOR BE LIABLE FOR
  * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,  OR CONSEQUENTIAL DAMAGES
  * (INCLUDING, BUT NOT LIMITED TO,  PROCUREMENT OF  SUBSTITUTE GOODS  OR SERVICES;
  * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER  CAUSED AND ON
- * ANY  THEORY OF  LIABILITY, WHETHER  IN  CONTRACT,  STRICT  LIABILITY,  OR  TORT 
+ * ANY  THEORY OF  LIABILITY, WHETHER  IN  CONTRACT,  STRICT  LIABILITY,  OR  TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN  ANY WAY  OUT OF THE USE OF THIS
  * PRODUCT, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  READER AND/OR USER
  * USES AS THEIR OWN RISK.
- * 
+ *
  * Any inaccuracy in source code, code comments, documentation, or other expressed
  * form within Product,  is unintentional and corresponding hardware specification
  * takes precedence.
- * 
+ *
  * Let it be known that  the purpose of this Product is to be used as supplemental
  * product for one or more of the following mentioned books.
- * 
+ *
  *   FYSOS: Operating System Design
  *    Volume 1:  The System Core
  *    Volume 2:  The Virtual File System
@@ -47,17 +47,17 @@
  *    Volume 6:  The Graphical User Interface
  *    Volume 7:  ** Not yet published **
  *    Volume 8:  USB: The Universal Serial Bus
- * 
+ *
  * This Product is  included as a companion  to one or more of these  books and is
  * not intended to be self-sufficient.  Each item within this distribution is part
  * of a discussion within one or more of the books mentioned above.
- * 
+ *
  * For more information, please visit:
  *             http://www.fysnet.net/osdesign_book_series.htm
  */
 
 /*
- *  Last updated: 5 Feb 2022
+ *  Last updated: 15 Nov 2022
  */
 
 // set it to 1 (align on byte)
@@ -74,7 +74,7 @@
 
 #define LOG2(x) (int) (log((double) (x)) / log(2.0))  // log2(x) = log(x) / log(2.0)
 
-char strtstr[] = "\nMLEANFS  v2.10.10    Forever Young Software 1984-2022\n\n";
+char strtstr[] = "\nMLEANFS  v2.10.20    Forever Young Software 1984-2022\n\n";
 
 // default superblock location (will sector pad to this location if boot_size is less than)
 //  if boot code is larger, will move super location to just past boot code
@@ -131,36 +131,36 @@ struct S_LEAN_INODE {
   uint32_t attributes;              // see table below
   uint64_t file_size;               // file size
   uint64_t block_count;             // count of blocks used
-  bit64s acc_time;                // last accessed: number of mS elapsed since midnight of 1970-01-01
-  bit64s sch_time;                // status change: number of mS elapsed since midnight of 1970-01-01
-  bit64s mod_time;                // last modified: number of mS elapsed since midnight of 1970-01-01
-  bit64s cre_time;                //       created: number of mS elapsed since midnight of 1970-01-01
+  bit64s acc_time;                  // last accessed: number of mS elapsed since midnight of 1970-01-01
+  bit64s sch_time;                  // status change: number of mS elapsed since midnight of 1970-01-01
+  bit64s mod_time;                  // last modified: number of mS elapsed since midnight of 1970-01-01
+  bit64s cre_time;                  //       created: number of mS elapsed since midnight of 1970-01-01
   uint64_t first_indirect;          // address of the first indirect block of the file.
   uint64_t last_indirect;           // address of the last indirect block of the file.
   uint64_t fork;
   uint64_t extent_start[LEAN_INODE_EXTENT_CNT]; // The array of extents
-  uint32_t extent_size[LEAN_INODE_EXTENT_CNT]; 
+  uint32_t extent_size[LEAN_INODE_EXTENT_CNT];
 };
 
 //attributes:
-#define  LEAN_ATTR_IXOTH        (1 << 0)  // Other: execute permission 
-#define  LEAN_ATTR_IWOTH        (1 << 1)  // Other: write permission 
-#define  LEAN_ATTR_IROTH        (1 << 2)  // Other: read permission 
-#define  LEAN_ATTR_IXGRP        (1 << 3)  // Group: execute permission 
-#define  LEAN_ATTR_IWGRP        (1 << 4)  // Group: write permission 
-#define  LEAN_ATTR_IRGRP        (1 << 5)  // Group: read permission 
-#define  LEAN_ATTR_IXUSR        (1 << 6)  // Owner: execute permission 
-#define  LEAN_ATTR_IWUSR        (1 << 7)  // Owner: write permission 
-#define  LEAN_ATTR_IRUSR        (1 << 8)  // Owner: read permission 
-//       LEAN_ATTR_             (1 << 9)  // reserved
+#define  LEAN_ATTR_IXOTH        (1 << 0)  // Other: execute permission
+#define  LEAN_ATTR_IWOTH        (1 << 1)  // Other: write permission
+#define  LEAN_ATTR_IROTH        (1 << 2)  // Other: read permission
+#define  LEAN_ATTR_IXGRP        (1 << 3)  // Group: execute permission
+#define  LEAN_ATTR_IWGRP        (1 << 4)  // Group: write permission
+#define  LEAN_ATTR_IRGRP        (1 << 5)  // Group: read permission
+#define  LEAN_ATTR_IXUSR        (1 << 6)  // Owner: execute permission
+#define  LEAN_ATTR_IWUSR        (1 << 7)  // Owner: write permission
+#define  LEAN_ATTR_IRUSR        (1 << 8)  // Owner: read permission
+#define  LEAN_ATTR_ISVTX        (1 << 9)  // Other: restrict rename/delete to owner
 #define  LEAN_ATTR_ISUID        (1 << 10) // Other: execute as user id
-#define  LEAN_ATTR_ISGID        (1 << 11) // Other: execute as group id 
-#define  LEAN_ATTR_HIDDEN       (1 << 12) // Don't show in directory listing 
-#define  LEAN_ATTR_SYSTEM       (1 << 13) // Warn that this is a system file 
-#define  LEAN_ATTR_ARCHIVE      (1 << 14) // File changed since last backup 
-#define  LEAN_ATTR_SYNC_FL      (1 << 15) // Synchronous updates 
-#define  LEAN_ATTR_NOATIME_FL   (1 << 16) // Don't update last access time 
-#define  LEAN_ATTR_IMMUTABLE_FL (1 << 17) // Don't move file blocks 
+#define  LEAN_ATTR_ISGID        (1 << 11) // Other: execute as group id
+//       LEAN_ATTR_             (1 << 12) // reserved
+#define  LEAN_ATTR_SYSTEM       (1 << 13) // Warn that this is a system file
+#define  LEAN_ATTR_ARCHIVE      (1 << 14) // File changed since last backup
+#define  LEAN_ATTR_SYNC_FL      (1 << 15) // Synchronous updates
+#define  LEAN_ATTR_NOATIME_FL   (1 << 16) // Don't update last access time
+#define  LEAN_ATTR_IMMUTABLE_FL (1 << 17) // Don't move file blocks
 #define  LEAN_ATTR_PREALLOC     (1 << 18) // Keep any preallocated blocks beyond fileSize when the file is closed
 #define  LEAN_ATTR_INLINEXTATTR (1 << 19) // Remaining bytes after the inode structure are reserved for inline extended attributes
 //       LEAN_ATTR_             (1 << 20)  // reserved
@@ -172,11 +172,11 @@ struct S_LEAN_INODE {
 //       LEAN_ATTR_             (1 << 26)  // reserved
 //       LEAN_ATTR_             (1 << 27)  // reserved
 //       LEAN_ATTR_             (1 << 28)  // reserved
-#define  LEAN_ATTR_IFMT         (7 << 29) // Bit mask to extract the file type 
-#define  LEAN_ATTR_IFREG        (1 << 29) // File type: regular file 
-#define  LEAN_ATTR_IFDIR        (2 << 29) // File type: directory 
-#define  LEAN_ATTR_IFLNK        (3 << 29) // File type: symbolic link 
-#define  LEAN_ATTR_IFFRK        (4 << 29) // File type: fork 
+#define  LEAN_ATTR_IFMT         (7 << 29) // Bit mask to extract the file type
+#define  LEAN_ATTR_IFREG        (1 << 29) // File type: regular file
+#define  LEAN_ATTR_IFDIR        (2 << 29) // File type: directory
+#define  LEAN_ATTR_IFLNK        (3 << 29) // File type: symbolic link
+#define  LEAN_ATTR_IFFRK        (4 << 29) // File type: fork
 
 
 #define LEAN_NAME_LEN_MAX  4068   // 255 is largest value allowed in rec_len * 16 bytes per record - 12 bytes for header
@@ -194,13 +194,14 @@ struct S_LEAN_DIRENTRY {
 #define LEAN_FT_REG   1 // File type: regular file
 #define LEAN_FT_DIR   2 // File type: directory
 #define LEAN_FT_LNK   3 // File type: symbolic link
-#define LEAN_FT_FRK   4 // File type: fork
+//      LEAN_FT_      4 // reserved
+#define LEAN_FT_DEL   5 // File type: deleted
 
 #define FOLDER_SIZE  15  // default sectors per sub folder (inode + FOLDER_SIZE)
 
 struct S_LEAN_EXTENT {
   uint64_t start[LEAN_INODE_EXTENT_CNT]; // The array of extents
-  uint32_t size[LEAN_INODE_EXTENT_CNT]; 
+  uint32_t size[LEAN_INODE_EXTENT_CNT];
 };
 
 #pragma pack(pop)
