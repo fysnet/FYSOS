e0bc08185f21112b65fef6c31d2d63ce69883921
Benjamin David Lunt <spamcollector@cableone.net>
2022-11-15 13:21:23 -0700

    Update mleanfs to match new specs

--- main/filesys/utils/include/misc.h
+++ main/filesys/utils/include/misc.h
@@ -4,12 +4,12 @@
  *                             Forever Young Software
  *                            fys [at] fysnet [dot] net
  *                              All rights reserved
- * 
+ *
  * Redistribution and use in source or resulting in  compiled binary forms with or
  * without modification, are permitted provided that the  following conditions are
  * met.  Redistribution in printed form must first acquire written permission from
  * copyright holder.
- * 
+ *
  * 1. Redistributions of source  code must retain the above copyright notice, this
  *    list of conditions and the following disclaimer.
  * 2. Redistributions in printed form must retain the above copyright notice, this
@@ -17,27 +17,27 @@
  * 3. Redistributions in  binary form must  reproduce the above copyright  notice,
  *    this list of  conditions and the following  disclaimer in the  documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE, DOCUMENTATION, BINARY FILES, OR OTHER ITEM, HEREBY FURTHER KNOWN
  * AS 'PRODUCT', IS  PROVIDED BY THE COPYRIGHT  HOLDER AND CONTRIBUTOR "AS IS" AND
  * ANY EXPRESS OR IMPLIED  WARRANTIES, INCLUDING, BUT NOT  LIMITED TO, THE IMPLIED
- * WARRANTIES  OF  MERCHANTABILITY  AND  FITNESS  FOR  A  PARTICULAR  PURPOSE  ARE 
+ * WARRANTIES  OF  MERCHANTABILITY  AND  FITNESS  FOR  A  PARTICULAR  PURPOSE  ARE
  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT  OWNER OR CONTRIBUTOR BE LIABLE FOR
  * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,  OR CONSEQUENTIAL DAMAGES
  * (INCLUDING, BUT NOT LIMITED TO,  PROCUREMENT OF  SUBSTITUTE GOODS  OR SERVICES;
  * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER  CAUSED AND ON
- * ANY  THEORY OF  LIABILITY, WHETHER  IN  CONTRACT,  STRICT  LIABILITY,  OR  TORT 
+ * ANY  THEORY OF  LIABILITY, WHETHER  IN  CONTRACT,  STRICT  LIABILITY,  OR  TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN  ANY WAY  OUT OF THE USE OF THIS
  * PRODUCT, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  READER AND/OR USER
  * USES AS THEIR OWN RISK.
- * 
+ *
  * Any inaccuracy in source code, code comments, documentation, or other expressed
  * form within Product,  is unintentional and corresponding hardware specification
  * takes precedence.
- * 
+ *
  * Let it be known that  the purpose of this Product is to be used as supplemental
  * product for one or more of the following mentioned books.
- * 
+ *
  *   FYSOS: Operating System Design
  *    Volume 1:  The System Core
  *    Volume 2:  The Virtual File System
@@ -47,11 +47,11 @@
  *    Volume 6:  The Graphical User Interface
  *    Volume 7:  ** Not yet published **
  *    Volume 8:  USB: The Universal Serial Bus
- * 
+ *
  * This Product is  included as a companion  to one or more of these  books and is
  * not intended to be self-sufficient.  Each item within this distribution is part
  * of a discussion within one or more of the books mentioned above.
- * 
+ *
  * For more information, please visit:
  *             http://www.fysnet.net/osdesign_book_series.htm
  */
@@ -64,15 +64,19 @@
   #define FSEEK(f, o, s) _fseeki64(f, (o), s)
   #define FTELL(f) _ftelli64(f)
 #elif defined DJGPP
-  #define _FILE_OFFSET_BITS  64  
+  #define _FILE_OFFSET_BITS  64
   #define FSEEK(f, o, s) fseek(f, (o), s)
   #define FTELL(f) ftell(f)
+#elif defined __LCC__
+  #define _FILE_OFFSET_BITS  64
+  #define FSEEK(f, o, s) fseek(f, (o), s)
+  #define FTELL(f) ((bit64u) ftell(f))
 #else
 # error 64 bit ftell(), fseek() not defined
 #endif
 
 // try to create a portable getche() function call
-#if defined(_MSC_VER) || defined(DJGPP)
+#if defined(_MSC_VER) || defined(DJGPP) || defined(__LCC__)
   static inline int getchecho() { int ret = getch(); putch(ret); return ret; }
 #else
   static inline int getchecho() { return getchar(); }
@@ -82,6 +86,8 @@
 #ifdef _MSC_VER
   #define LL64BIT "I64"
 #elif defined DJGPP
+  #define LL64BIT "ll"
+#elif defined __LCC__
   #define LL64BIT "ll"
 #endif
 
@@ -139,10 +145,10 @@ struct S_GUID {
 //  However, this allows a user to track a guid to the machine that
 //  made the guid.
 void calc_guid(struct S_GUID *guid) {
-  
+
   // time() only returns 32-bits, so we need the cast below
   bit64u timestamp;
-  
+
   time((time_t *) &timestamp);
   guid->data1 = (bit32u) timestamp;
   guid->data2 = (bit16u) (timestamp >> 32);
@@ -177,14 +183,14 @@ struct S_RESOURCE {
 void get_a_string(FILE *fp, char *str) {
   char ch, *t = str;
   int c = NAME_LEN_MAX - 1;
-  
+
   // skip all leading spaces
   while (!feof(fp)) {
     ch = fgetc(fp);
     if (ch != ' ')
       break;
   }
-  
+
   while (1) {
     if (feof(fp))
       break;
@@ -199,7 +205,7 @@ void get_a_string(FILE *fp, char *str) {
       break;
     ch = fgetc(fp);
   }
-  
+
   // if it was the '#' char, skip to eol
   if (ch == '#') {
     while (!feof(fp)) {
@@ -208,7 +214,7 @@ void get_a_string(FILE *fp, char *str) {
         break;
     }
   }
-  
+
   // kill all trailing spaces
   while (t > str) {
     t--;
@@ -217,14 +223,14 @@ void get_a_string(FILE *fp, char *str) {
       break;
     }
   }
-  
+
   // asciiz it
   *t = 0;
 }
 
 /*
  *  # line is a comment
- *  mbrfile=filename   - This is the path/filename of the mbr file to use 
+ *  mbrfile=filename   - This is the path/filename of the mbr file to use
  *  bootfile=filename  - This is the path/filename of the boot code file to use
  *  imgfile=filename   - This is the path/filename of the target file to create/modify
  *  base_lba=0         - Base LBA to write the Super Block to
@@ -238,17 +244,17 @@ struct S_RESOURCE *parse_resource(const char *filename) {
   int limit = 10;  // start with allowing 10 pairs
   char str[NAME_LEN_MAX];
   FILE *fp;
-  
+
   if ((fp = fopen(filename, "r")) == NULL) {
     printf("\nError opening resource file.");
     return NULL;
   }
-  
+
   r = (struct S_RESOURCE *) calloc(sizeof(struct S_RESOURCE) - sizeof(r->files) + (limit * sizeof(r->files)), 1);
   r->base_lba = PART_START;       // assuming none given
   r->tot_sectors = (20*16*63);    // default size
-  r->param0 = 12;                 // 
-  
+  r->param0 = 12;                 //
+
   while (!feof(fp)) {
     get_a_string(fp, str);
     if (strcmp(str, "mbrfile") == 0) {
@@ -290,10 +296,10 @@ struct S_RESOURCE *parse_resource(const char *filename) {
       }
     }
   }
-  
+
   // close the file
   fclose(fp);
-  
+
   r->file_cnt = cnt;
   return r;
 }
